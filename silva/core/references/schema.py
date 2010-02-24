03352672e90a07cc107a8f5f9645fdda0d927a33
Sylvain Viollon <thefunny@gmail.com>
2010-02-24 14:30:51 +0000

    Add some schema.

---
+++ silva/core/references/schema.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from zope.schema import interfaces
+from zope import schema
+
+
+class IReference(interfaces.IObject):
+    """A reference field is like an object.
+    """
+
+
+class Reference(schema.Field):
+    """Store a reference to an object.
+    """
+
+    def _validate(self, value):
+        # No validation for the moment
+
