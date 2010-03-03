db9787622cc4f41784b8f473b34b66d5c5169918
Sylvain Viollon <thefunny@gmail.com>
2010-03-03 12:47:59 +0000

    Make widget work enough to continue the reference engine.

--- silva/core/references/widgets/static/reference-ui.js
+++ silva/core/references/widgets/static/reference-ui.js
@@ -1,5 +1,6 @@
-var ContentList = function(element) {
+var ContentList = function(element, widget_id) {
     this.element = $(element);
+    this.widget_id = widget_id;
 };
 
 ContentList.prototype.populate = function(url) {
@@ -10,26 +11,51 @@ ContentList.prototype.populate = function(url) {
                     var child = new ContentListItem(self,
                                                     url + '/' + entry['id'],
                                                     entry['title'],
-                                                    entry['type']);
+                                                    entry['type'],
+                                                    entry['icon'],
+                                                    entry['intid']);
                     child.build(self.element);
                 });
         });
 };
 
-var ContentListItem = function(content_list, url, title, type) {
+var ContentListItem = function(content_list, url, title, type, icon, intid) {
     this.content_list = content_list;
     this.url = url;
     this.title = title;
     this.type = type;
+    this.icon = icon;
+    this.intid = intid;
 };
 
 ContentListItem.prototype.build = function(root) {
     var self = this;
+    var link = $('<a />');
+    var icon = $('<img />');
+    var use = $('<input type="checkbox" />');
     var item = $('<li />');
-    item.text(self.title);
-    item.click(function(event){
+
+    link.text(self.title);
+    link.click(function(event){
             self.content_list.populate.apply(self.content_list, [self.url]);
+            return false;
         });
+    link.prependTo(item);
+
+    icon.attr('src', self.icon);
+    icon.prependTo(item);
+
+    use.click(function(event){
+        var content_link = $('#' + self.content_list.widget_id + '-link');
+        content_link.text(self.title);
+        content_link.attr('href', self.url);
+        var content_value = $('#' + self.content_list.widget_id + '-value');
+        content_value.attr('value', self.intid);
+        var popup = $('#' + self.content_list.widget_id + '-dialog');
+        popup.dialog('close');
+    });
+    use.prependTo(item);
+
     item.appendTo(root);
 };
 
@@ -37,19 +63,20 @@ $(document).ready(function() {
 
    $('.reference-dialog').dialog({
         autoOpen: false,
-            modal: true,
-            buttons: {
+        modal: true,
+        height: 500,
+        buttons: {
             'Cancel': function() {
                 $(this).dialog('close');
             }
-        },
+        }
    });
 
    $('.reference-dialog-trigger').click(function() {
         var url = 'http://localhost:8080/silva/docs';
         var widget = $(this).parent('.reference-widget');
         var popup = $('#' + widget.attr('id') + '-dialog');
-        var content_list = new ContentList(popup);
+        var content_list = new ContentList(popup, widget.attr('id'));
         content_list.populate(url);
         popup.dialog('open');
         return false;
