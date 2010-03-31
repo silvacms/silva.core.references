03694099612edef8da2c3b043a34764676f6d0c9
Antonin Amand <antonin.amand@gmail.com>
2010-03-31 09:49:43 +0000

    add breadcrumb path to reference lookup widget

--- silva/core/references/widgets/static/reference-ui.js
+++ silva/core/references/widgets/static/reference-ui.js
@@ -2,7 +2,18 @@ var ContentList = function(element, widget_id) {
     this.id = widget_id;
     this.element = $(element);
     this.reference_interface = $('#' + this.id + '-interface');
+    this.parent = null;
+    this.current = null;
 };
+
+function extract_from_id(data, id) {
+    for(var i=0; i < data.length; i++) {
+        if (data[i]['id'] == id) {
+            return data.splice(i, 1)[0];
+        }
+    }
+    return null;
+}
 
 ContentList.prototype.populate = function(url) {
     var self = this;
@@ -11,14 +22,35 @@ ContentList.prototype.populate = function(url) {
     if (this.reference_interface.val()) {
         options['interface'] = this.reference_interface.val();
     };
+
     $.getJSON(url + '/++rest++items', options, function (data) {
-            self.element.empty();
-            $.each(data, function(index, entry) {
-                    var child = new ContentListItem(
-                        self, url + '/' + entry['id'], entry);
-                    child.build(self.element);
-                });
+        self.element.empty();
+        var root = $('<ul class="content_list" />');
+        root.appendTo(self.element);
+        // get rid of '.' and '..'
+        self.current = extract_from_id(data, '.');
+        self.parent = extract_from_id(data, '..');
+        self.build_path.apply(self);
+        $.each(data, function(index, entry) {
+                var child = new ContentListItem(
+                    self, url + '/' + entry['id'], entry);
+                child.build(root);
         });
+    });
+};
+
+ContentList.prototype.build_path = function() {
+    var self = this;
+    var element = this.element.find("div.path_list");
+    if (element.length == 0) {
+        element = $('<div class="path_list" />');
+    }
+    var path_list = new PathList(element);
+    this.element.prepend(element);
+    path_list.fetch(this.current);
+    path_list.bind(function(event, data){
+        self.populate.apply(self, [data['url']]);
+    });
 };
 
 var ContentListItem = function(
@@ -30,30 +62,38 @@ var ContentListItem = function(
 
 ContentListItem.prototype.build = function(root) {
     var self = this;
-    var link = $('<a />');
     var icon = $('<img />');
-    var use = $('<input type="checkbox" />');
     var item = $('<li />');
+    var link = null
+    if (self.info['folderish']) {
+        link = $('<a href="#" />');
+        link.click(function(event){
+                self.content_list.populate.apply(self.content_list, [self.url]);
+                return false;
+            });
+    } else {
+        link = $('<span />');
+    }
 
+    link.data('++rest++', self.info);
     link.text(self.info['title']);
-    link.click(function(event){
-            self.content_list.populate.apply(self.content_list, [self.url]);
-            return false;
-        });
     link.prependTo(item);
 
     icon.attr('src', self.info['icon']);
     icon.prependTo(item);
 
-    use.click(function(event){
-        var id = self.content_list.id;
-        var popup = $('#' + id  + '-dialog');
-        var reference = new ReferencedRemoteObject(id);
+    if (self.info['implements']) {
+        var use = $('<input type="checkbox" />');
+        use.click(function(event){
+            var id = self.content_list.id;
+            var popup = $('#' + id  + '-dialog');
+            var reference = new ReferencedRemoteObject(id);
 
-        reference.render(self.info);
-        popup.dialog('close');
-    });
-    use.prependTo(item);
+            reference.render(self.info);
+            popup.dialog('close');
+        });
+        use.prependTo(item);
+    }
 
     item.appendTo(root);
 };
@@ -169,12 +209,97 @@ ReferencedRemoteObject.prototype.toggle = function() {
     this.widget.toggle();
 };
 
+var PathList = function(node, options) {
+    this.node = $(node);
+    default_options = {
+    // link text
+    'display_field': 'title',
+    // link title attribute
+    'title_field': 'id',
+    // max items to display besides root
+    'max_items': 3};
+    options = options || {};
+    this.options = {};
+    $.extend(this.options, default_options, options);
+};
+
+PathList.prototype.fetch = function(info) {
+    var self = this;
+    var url = info['url'];
+    $.getJSON(url + '/++rest++parents',
+                function(data) {
+                    self.render.apply(self, [data]);
+                });
+};
+
+PathList.prototype.render = function(data) {
+    var len = data.length;
+    var max_items = this.options['max_items'];
+    var stop = data.length;
+    var start = 1;
+    this.node.empty();
+    this.node.append(this._build_entry(data[0]));
+
+    if (max_items != 0 && len > (max_items + 1)) {
+        start = len - max_items;
+        this.node.append(this._build_fake());
+    }
+
+    for(var i = start; i < stop; i++) {
+        this.node.append(this._build_entry(data[i]));
+    }
+};
+
+PathList.prototype._build_fake = function() {
+    var outer = $('<span class="path_outer" />');
+    var inner = $('<span class="path_inner" />');
+    // var img = $('<img />')
+    var link = $('<span class="path_fake" />');
+    link.text('...');
+    link.appendTo(inner);
+    inner.appendTo(outer);
+    return outer;
+};
+
+PathList.prototype._build_entry = function(info) {
+    var self = this;
+    var outer = $('<span class="path_outer" />');
+    var inner = $('<span class="path_inner" />');
+    // var img = $('<img />')
+    var link = $('<a href="#" />');
+    link.attr('title', info[this.options['title_field']])
+    link.text(info[this.options['display_field']]);
+    link.data('++rest++', info);
+
+    link.click(function(event){
+        var data = $(this).data('++rest++');
+        self._notify(event, data);
+        return false;
+    });
+
+    // img.attr('src', info['icon']);
+    // link.prepend(img);
+
+    link.appendTo(inner);
+    inner.appendTo(outer);
+    return outer;
+}
+
+PathList.prototype._notify = function(event, data) {
+  this.node.trigger('path-modified', data);
+}
+
+PathList.prototype.bind = function(callback) {
+    this.node.bind('path-modified', callback);
+}
+
 $(document).ready(function() {
 
    $('.reference-dialog').dialog({
         autoOpen: false,
         modal: true,
         height: 500,
+        width: 600,
         buttons: {
             'Cancel': function() {
                 $(this).dialog('close');
